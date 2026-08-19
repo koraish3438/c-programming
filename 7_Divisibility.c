eae029f9aa9806b0836de70ebec47d0ee9bc335d
koraish3438 <koraishmdkajamall38@gmail.com>
2026-08-19 14:48:08 +0600

    solve divisibility

---
+++ 7_Divisibility.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+
+    if(a % b == 0) {
+        printf("Divisible");
+    }
+    else {
+        printf("Not Divisible");
+    }
+
+    return 0;
+}
